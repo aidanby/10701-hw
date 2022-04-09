cc89c565ea577ff457ed37e6ba2c4f5708fa372c
aidanbyang@hotmail.com <aidanbyang@hotmail.com>
2022-04-09 11:42:39 -0400

    5.1 pics

--- hw4/AdaBoost.py
+++ hw4/AdaBoost.py
@@ -14,7 +14,7 @@ def read_data(file):
 
 
 f = None
-description = None
+description = []
 boundary = 0
 y_pred = None
 accuracy = 0
@@ -38,10 +38,14 @@ def ds_fit(train_x, train_y, weights=None):
             lowest_err = err1
             description = "vertical <{} -1".format(
                 boundary)  # horizontal axis < bound is classified as -1
+
+            description = ['vert', boundary]
+
             f = lambda x: (-1 if x[0] < boundary else 1)
         if err2 < lowest_err:
             lowest_err = err2
             description = "vertical <{} 1".format(boundary)
+            description = ['vert', boundary]
             f = lambda x: (1 if x[0] < boundary else -1)
         # Vertical
         bound = v[i][0] if i < len(v) else float("inf")
@@ -52,10 +56,12 @@ def ds_fit(train_x, train_y, weights=None):
         if err1 < lowest_err:
             lowest_err = err1
             description = "horizontal <{} -1".format(boundary)
+            description = ['hor', boundary]
             f = lambda x: (-1 if x[1] < boundary else 1)
         if err2 < lowest_err:
             lowest_err = err2
             description = "horizontal <{} 1".format(boundary)
+            description = ['hor', boundary]
             f = lambda x: (1 if x[1] < boundary else -1)
     accuracy = 1 - lowest_err / len(train_y)
     return f
@@ -100,11 +106,13 @@ class DecisionStump:
                 self.boundary = bound
                 self.description = "vertical <{} -1".format(
                     self.boundary)  # horizontal axis < bound is classified as -1
+                self.description = ['vert', self.boundary]
                 self.f = lambda x: (-1 if x[0] < self.boundary else 1)
             if err2 < lowest_err:
                 lowest_err = err2
                 self.boundary = bound
                 self.description = "vertical <{} 1".format(self.boundary)
+                self.description = ['vert', self.boundary]
                 self.f = lambda x: (1 if x[0] < self.boundary else -1)
             # Vertical
             bound = v[i][0] if i < len(v) else float("inf")
@@ -116,16 +124,19 @@ class DecisionStump:
                 lowest_err = err1
                 self.boundary = bound
                 self.description = "horizontal <{} -1".format(self.boundary)
+                self.description = ['hor', self.boundary]
                 self.f = lambda x: (-1 if x[1] < self.boundary else 1)
             if err2 < lowest_err:
                 lowest_err = err2
                 self.description = "horizontal <{} 1".format(self.boundary)
+                self.description = ['hor', self.boundary]
                 self.boundary = bound
                 self.f = lambda x: (1 if x[1] < self.boundary else -1)
         self.accuracy = 1 - lowest_err / len(train_y)
         return self.f
 
     def predict(self, test_x):
+        a = self.f
         self.y_pred = np.array(list(map(self.f, test_x)))
         return self.y_pred
 
@@ -222,6 +233,49 @@ class AdaBoost:
         self.accuracy = np.sum(np.abs(y + y_pred)) / (2 * y.shape[0])
         return self.accuracy
 
+    def plot_weights(self, model, train_y, train_x, weights):
+        pos_x = train_x[train_y == 1]
+        neg_x = train_x[train_y == -1]
+        weights = weights ** 2 * 100
+        pos_weights = weights[train_y == 1]
+        neg_weights = weights[train_y == -1]
+        model.description
+        db = model.description[1]
+
+        plt.figure(figsize=(6, 6))
+        plt.scatter(neg_x[:, 0], neg_x[:, 1], s=neg_weights, color='red', marker='_')
+        plt.scatter(pos_x[:, 0], pos_x[:, 1], s=pos_weights, color='blue', marker='+')
+
+        plt.title('Weighted Data', fontsize=16)
+        plt.legend(['-1', '+1'], fontsize=16)
+        plt.xlabel('$x_1$', fontsize=16)
+        plt.ylabel('$x_2$', fontsize=16)
+        plt.show()
+
+        if 'vert' in model.description[0]:
+            print('hi')
+            plt.figure(figsize=(6, 6))
+            plt.scatter(neg_x[:, 0], neg_x[:, 1], s=neg_weights, color='red', marker='_')
+            plt.scatter(pos_x[:, 0], pos_x[:, 1], s=pos_weights, color='blue', marker='+')
+            plt.plot([db, db], [-2, 2], color='black', label='_nolegend_')
+            plt.fill([-2,-2, db, db], [-2, 2, 2, -2], color='red', alpha=0.1, label='_nolegend_')
+            plt.fill([2, 2, db, db], [-2, 2, 2, -2], color='blue', alpha=0.1, label='_nolegend_')
+        else:
+            plt.figure(figsize=(6, 6))
+            plt.scatter(neg_x[:, 0], neg_x[:, 1], s=neg_weights, color='red', marker='_')
+            plt.scatter(pos_x[:, 0], pos_x[:, 1], s=pos_weights, color='blue', marker='+')
+            plt.plot([-2, 2], [db, db], color='black', label='_nolegend_')
+            plt.fill([-2,-2, 2, 2], [-2, db, db, -2], color='red', alpha=0.1, label='_nolegend_')
+            plt.fill([-2,-2, 2, 2], [2, db, db, 2], color='blue', alpha=0.1, label='_nolegend_')
+
+        plt.title('Weighted Data w/ Tree', fontsize=16)
+        plt.legend(['-1', '+1'], fontsize=16)
+        plt.xlabel('$x_1$', fontsize=16)
+        plt.ylabel('$x_2$', fontsize=16)
+        plt.show()
+        print('here')
+
+
     def plot_result(self, train_x, train_y, test_x, test_y, iteration=1):
         plt.figure()
         accuracy = []
@@ -234,6 +288,11 @@ class AdaBoost:
             train_y_pred = model.predict(train_x)
             err = np.sum(self.weights * np.abs(train_y - train_y_pred) / 2) / np.sum(self.weights)
             vote = np.log((1 - err) / err) / 2
+
+            # 5.1
+            self.plot_weights(model, train_y_pred, train_x, self.weights)
+
+
             self.weights = self.weights * np.exp(-train_y * vote * train_y_pred)  # update weights
             self.votes.append(vote)
             self.learners.append(model)
@@ -250,9 +309,10 @@ class AdaBoost:
 if __name__ == "__main__":
     train_x, train_y = read_data("data_22/train_adaboost.csv")
     test_x, test_y = read_data("data_22/test_adaboost.csv")
-    # AB = AdaBoost()
-    plot_result(train_x, train_y, test_x, test_y, iteration=50)  # 5.1 plot
-    # print(AB.accuracy)  # 5.2 test accuracy after 50 iterations
-    # print(AB.learners[0].description)  # 5.3
-    # print(AB.learners[1].description)
-    # print(AB.learners[2].description)
+    AB = AdaBoost()
+    AB.plot_result(train_x, train_y, test_x, test_y, iteration=50)  # 5.1 plot
+    # plot_result(train_x, train_y, test_x, test_y, iteration=50)  # 5.1 plot
+    print(AB.accuracy)  # 5.2 test accuracy after 50 iterations
+    print(AB.learners[0].description)  # 5.3
+    print(AB.learners[1].description)
+    print(AB.learners[2].description)
