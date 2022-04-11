b9eeaac9d715146f277bba6de03c284764f89401
aidanbyang@hotmail.com <aidanbyang@hotmail.com>
2022-04-11 18:34:42 -0400

    changes

--- hw4/ada_boost.py
+++ hw4/ada_boost.py
@@ -10,63 +10,9 @@ def read_data(file):
     return data_x, data_y
 
 
-f = None
 description = []
 boundary = 0
-y_pred = None
-accuracy = 0
-
-
-def ds_fit(train_x, train_y, weights=None):
-    if weights is None:
-        weights = np.ones_like(train_y)
-    h = [(x[0], y) for (x, y) in zip(train_x, train_y)]
-    v = [(x[1], y) for (x, y) in zip(train_x, train_y)]
-    # Compute horizontal boundary
-    lowest_err = len(train_y)
-    for i in range(len(h) + 1):
-        # Horizontal
-        bound = h[i][0] if i < len(h) else float("inf")
-        f1 = lambda x: 1 if ((x[0] < bound and x[1] > 0) or (x[0] >= bound and x[1] < 0)) else 0
-        f2 = lambda x: 1 if ((x[0] < bound and x[1] < 0) or (x[0] >= bound and x[1] > 0)) else 0
-        err1 = np.sum(weights * np.array(list(map(f1, h))))
-        err2 = np.sum(weights * np.array(list(map(f2, h))))
-        if err1 < lowest_err:
-            lowest_err = err1
-            description = ['vert', boundary]
-
-            f = lambda x: (-1 if x[0] < boundary else 1)
-        if err2 < lowest_err:
-            lowest_err = err2
-            description = ['vert', boundary]
-            f = lambda x: (1 if x[0] < boundary else -1)
-        # Vertical
-        bound = v[i][0] if i < len(v) else float("inf")
-        f1 = lambda x: 1 if ((x[0] < bound and x[1] > 0) or (x[0] >= bound and x[1] < 0)) else 0
-        f2 = lambda x: 1 if ((x[0] < bound and x[1] < 0) or (x[0] >= bound and x[1] > 0)) else 0
-        err1 = np.sum(weights * np.array(list(map(f1, v))))
-        err2 = np.sum(weights * np.array(list(map(f2, v))))
-        if err1 < lowest_err:
-            lowest_err = err1
-            description = ['hor', boundary]
-            f = lambda x: (-1 if x[1] < boundary else 1)
-        if err2 < lowest_err:
-            lowest_err = err2
-            description = ['hor', boundary]
-            f = lambda x: (1 if x[1] < boundary else -1)
-    return f
-
-
-def ds_predict(f, test_x):
-    return np.array(list(map(f, test_x)))
-
-
-def compute_error(y, y_pred):
-    return np.sum(np.abs(y + y_pred)) / (2 * y.shape[0])
-
-
 iteration = 0
-learners = []
 weighted_error = []
 votes = []
 weights = None
@@ -77,23 +23,57 @@ accuracy = 0
 def ada_fit(train_x, train_y, iteration=1):
     weights = np.ones(train_y.shape[0])
     for k in range(iteration):
-        f = ds_fit(train_x, train_y, weights)
-        train_y_pred = ds_predict(train_x)
+        if weights is None:
+            weights = np.ones_like(train_y)
+        h = [(x[0], y) for (x, y) in zip(train_x, train_y)]
+        v = [(x[1], y) for (x, y) in zip(train_x, train_y)]
+        lowest_err = len(train_y)
+        for i in range(len(h) + 1):
+            bound = h[i][0] if i < len(h) else float("inf")
+            f1 = lambda x: 1 if ((x[0] < bound and x[1] > 0) or (x[0] >= bound and x[1] < 0)) else 0
+            f2 = lambda x: 1 if ((x[0] < bound and x[1] < 0) or (x[0] >= bound and x[1] > 0)) else 0
+            err1 = np.sum(weights * np.array(list(map(f1, h))))
+            err2 = np.sum(weights * np.array(list(map(f2, h))))
+            if err1 < lowest_err:
+                lowest_err = err1
+                description = ['vert', boundary]
+                f = lambda x: (-1 if x[0] < boundary else 1)
+            if err2 < lowest_err:
+                lowest_err = err2
+                description = ['vert', boundary]
+                f = lambda x: (1 if x[0] < boundary else -1)
+            bound = v[i][0] if i < len(v) else float("inf")
+            f1 = lambda x: 1 if ((x[0] < bound and x[1] > 0) or (x[0] >= bound and x[1] < 0)) else 0
+            f2 = lambda x: 1 if ((x[0] < bound and x[1] < 0) or (x[0] >= bound and x[1] > 0)) else 0
+            err1 = np.sum(weights * np.array(list(map(f1, v))))
+            err2 = np.sum(weights * np.array(list(map(f2, v))))
+            if err1 < lowest_err:
+                lowest_err = err1
+                description = ['hor', boundary]
+                f = lambda x: (-1 if x[1] < boundary else 1)
+            if err2 < lowest_err:
+                lowest_err = err2
+                description = ['hor', boundary]
+                f = lambda x: (1 if x[1] < boundary else -1)
+        train_y_pred = np.array(list(map(f, test_x)))
         err = np.sum(weights * np.abs(train_y - train_y_pred) / 2) / np.sum(weights)
         vote = np.log((1 - err) / err) / 2
         weights = weights * np.exp(-train_y * vote * train_y_pred)  # update weights
         votes.append(vote)
         learners.append(f)
 
+        return vote, f, weights, err
+
 
 def ada_predict(test_x):
     s = np.zeros(test_x.shape[0])
     for k in range(iteration):
-        s += votes[k] * ds_predict(learners[k], test_x)
+        s += votes[k] * np.array(list(map(f, test_x)))
     return np.array(list(map(lambda x: -1 if x < 0 else 1, s / sum(votes))))
 
 
 def plot_weights(model, train_y, train_x, weights):
+    # 5.1 plots
     pos_x = train_x[train_y == 1]
     neg_x = train_x[train_y == -1]
     weights = weights ** 2 * 100
@@ -135,21 +115,23 @@ def plot_weights(model, train_y, train_x, weights):
     print('here')
 
 
-def plot_result(train_x, train_y, test_x, test_y, iteration=1):
+def plot_acc(train_x, train_y, test_x, test_y, iteration=1):
+    # 5.2 plots
     plt.figure()
     accuracy = []
     iteration = 0
     weights = np.ones(train_y.shape[0])
     for k in range(iteration):
         iteration = k + 1
-        ada_fit(train_x, train_y, weights)
+        vote, f, weights, err = ada_fit(train_x, train_y, weights)
         train_y_pred = ada_predict(train_x)
         err = np.sum(weights * np.abs(train_y - train_y_pred) / 2) / np.sum(weights)
         vote = np.log((1 - err) / err) / 2
         weights = weights * np.exp(-train_y * vote * train_y_pred)
+        plot_weights(f, train_y, train_x, weights)
         votes.append(vote)
         test_y_pred = ada_predict(test_x)
-        accuracy.append(compute_error(test_y, test_y_pred))
+        accuracy.append(np.sum(np.abs(y + y_pred)) / (2 * y.shape[0]))
     plt.plot([i + 1 for i in range(iteration)], accuracy)
     plt.xlabel("Number of iterations")
     plt.ylabel("Accuracy")
@@ -158,7 +140,25 @@ def plot_result(train_x, train_y, test_x, test_y, iteration=1):
     return accuracy
 
 
+def pca_analysis():
+    # 4.2 matrix calculations
+    x = [[-2, -2, -2, 0, 0], [-2, -2, -2, 0, 0], [4, 4, 4, 0, 0], [0, 0, 0, -2, -2], [0, 0, 0, 2, 2], [0, 0, 0, 0, 0]]
+    import sklearn
+    from sklearn.decomposition import PCA
+    y = np.linalg.svd(x)
+    print(np.rint(y[0]))
+    print(np.rint(y[1]))
+    print(np.rint(y[2]))
+    pca = PCA(n_components=1)
+    a = pca.fit_transform(x)
+    print(np.var(a))
+    print(np.linalg.norm(x - pca.inverse_transform(a)))
+
+
 if __name__ == "__main__":
     train_x, train_y = read_data("data_22/train_adaboost.csv")
     test_x, test_y = read_data("data_22/test_adaboost.csv")
-    plot_result(train_x, train_y, test_x, test_y, iteration=50)
+    plot_acc(train_x, train_y, test_x, test_y, iteration=50)
+
+    # pca_analysis()
+
--- hw4/em_gmm.py
+++ hw4/em_gmm.py
@@ -16,34 +16,38 @@ $f(\mathbf{x}, \boldsymbol{\mu}, \Sigma) = (2\pi)^{-M/2}|\Sigma|^{-1/2}~e^{\frac
 
 """
 
+
 def gaussian(x, mu, cov):
     # x and mu should be vectors in numpy, shape=(2,)
     # cov should be a matrix in numpy, shape=(2,2)
     M = 2
-    scale = (2*np.pi)**(-M/2)*np.linalg.det(cov)**(-1/2)
-    return scale*np.exp(-(1/2)*(x-mu).T @ np.linalg.inv(cov) @ (x-mu))
+    scale = (2 * np.pi) ** (-M / 2) * np.linalg.det(cov) ** (-1 / 2)
+    return scale * np.exp(-(1 / 2) * (x - mu).T @ np.linalg.inv(cov) @ (x - mu))
+
 
 """## Plot Gaussian contours function"""
+
 
 def plot_gaussian(mu, cov, x1_min=-10, x1_max=10, x2_min=-10, x2_max=10, color=None):
     x1_values = np.linspace(x1_min, x1_max, 101)
     x2_values = np.linspace(x2_min, x2_max, 101)
 
-    x1_grid, x2_grid = np.meshgrid(x1_values,x2_values)
+    x1_grid, x2_grid = np.meshgrid(x1_values, x2_values)
 
-    M,N = x1_grid.shape
-    y_grid = np.zeros((M,N))
+    M, N = x1_grid.shape
+    y_grid = np.zeros((M, N))
 
     x = np.zeros((2,))
 
     for i in range(M):
         for j in range(N):
-            x[0] = x1_grid[i,j]
-            x[1] = x2_grid[i,j]
+            x[0] = x1_grid[i, j]
+            x[1] = x2_grid[i, j]
 
-            y_grid[i,j] = gaussian(x, mu, cov)
+            y_grid[i, j] = gaussian(x, mu, cov)
 
     plt.contour(x1_grid, x2_grid, y_grid, colors=color)
+
 
 """## Load data
 Note: The code assumes that the data file is in the same folder as the jupyter notebook. In Google colab, you can upload the file directly into the workspace by in the Files tab on the left.
@@ -54,26 +58,23 @@ print(X.shape)
 
 """## Initial parameters"""
 
-K=4
-
+K = 4
 mu_list = []
 sigma_list = []
 pi_list = []
 
 for k in range(K):
-    mu_list.append((k+1)*np.ones((2,)))
-
+    mu_list.append((k + 1) * np.ones((2,)))
     sigma_list.append(np.eye(2))
-
-    pi_list.append(1/K)
+    pi_list.append(1 / K)
 
 """## Plot data with initial Gaussian contours"""
 
 # Square figure size
-plt.figure(figsize=(8,8))
+plt.figure(figsize=(8, 8))
 
 # Plot points
-plt.plot(X[:,0], X[:,1], 'o', markerfacecolor="None", alpha=0.3)
+plt.plot(X[:, 0], X[:, 1], 'o', markerfacecolor="None", alpha=0.3)
 
 # Plot K Gaussians
 colors = ['tab:orange', 'tab:green', 'tab:red', 'tab:purple']
@@ -89,140 +90,70 @@ plt.xlabel("$x_1$", fontsize=20)
 plt.ylabel("$x_2$", fontsize=20)
 plt.title("Iteration 0", fontsize=20)
 
-def compute_responsibilities(X, pi_list, mu_list, sigma_list):
-  '''E-step: compute responsibilities, given the current parameters'''
-  num_data = len(X)
-  resp = np.zeros((num_data, K))
-  # Update resp matrix so that resp[i,k] is the responsibility of cluster k for data point i.
-  # Hint: To compute likelihood of seeing data point i given cluster k, use multivariate_normal.pdf.
-  for i in range(num_data):
-      for j in range(K):
-          resp[i][j] = pi_list[j]*gaussian(X[i], mu_list[j], sigma_list[j])
 
-  # Add up responsibilities over each data point and normalize
-  row_sums = resp.sum(axis=1)[:, np.newaxis]
-  resp = resp / row_sums
-  
-  return resp
+def e_step(X, pi_list, mu_list, sigma_list):
+    num_data = len(X)
+    ret = np.zeros((num_data, K))
+    for i in range(num_data):
+        for j in range(K):
+            ret[i][j] = pi_list[j] * gaussian(X[i], mu_list[j], sigma_list[j])
+    return ret / ret.sum(axis=1)[:, np.newaxis]
 
-# M-step
-def compute_soft_counts(resp):
-    # Compute the total responsibility assigned to each cluster, which will be useful when 
-    # implementing M-steps below. In the lectures this is called N^{soft}
-    counts = np.sum(resp, axis=0)
-    return counts
 
-def compute_weights(counts):
-    num_clusters = len(counts)
-    weights = [0.] * num_clusters
-    
-    for k in range(num_clusters):
-        # Update the weight for cluster k using the M-step update rule for the cluster weight, \hat{\pi}_k.
-        # HINT: compute # of data points by summing soft counts.
-        # YOUR CODE HERE
-        weights[k] = counts[k] / np.sum(counts)
-
-    return weights
-
-def compute_means(data, resp, counts):
-    num_clusters = len(counts)
-    num_data = len(data)
-    means = [np.zeros(len(data[0]))] * num_clusters
-    
-    for k in range(num_clusters):
-        # Update means for cluster k using the M-step update rule for the mean variables.
-        # This will assign the variable means[k] to be our estimate for \hat{\mu}_k.
-        weighted_sum = 0.
-        for i in range(num_data):
-            # YOUR CODE HERE
-            weighted_sum += data[i] * resp[i][k]
-        # YOUR CODE HERE
-        means[k] = weighted_sum / counts[k]
-
-    return means
-
-def compute_covariances(data, resp, counts, means):
-    num_clusters = len(counts)
-    num_dim = len(data[0])
-    num_data = len(data)
-    covariances = [np.zeros((num_dim,num_dim))] * num_clusters
-    
-    for k in range(num_clusters):
-        # Update covariances for cluster k using the M-step update rule for covariance variables.
-        # This will assign the variable covariances[k] to be the estimate for \hat{\Sigma}_k.
-        weighted_sum = np.zeros((num_dim, num_dim))
-        for i in range(num_data):
-            # YOUR CODE HERE (Hint: Use np.outer on the data[i] and this cluster's mean)
-            weighted_sum += resp[i][k]*np.outer(data[i] - means[k], data[i] - means[k])
-        # YOUR CODE HERE
-        covariances[k] = weighted_sum / counts[k]
-    return covariances
-
-def log_sum_exp(Z):
-    """ Compute log(\sum_i exp(Z_i)) for some array Z."""
-    return np.max(Z) + np.log(np.sum(np.exp(Z - np.max(Z))))
-
-def loglikelihood(data, weights, means, covs):
-    """ Compute the loglikelihood of the data for a Gaussian mixture model with the given parameters. """
-    num_clusters = len(means)
-    num_dim = len(data[0])
-    
-    ll = 0
-    for d in data:
-        
-        Z = np.zeros(num_clusters)
-        for k in range(num_clusters):
-            
-            # Compute (x-mu)^T * Sigma^{-1} * (x-mu)
-            delta = np.array(d) - means[k]
-            exponent_term = np.dot(delta.T, np.dot(np.linalg.inv(covs[k]), delta))
-            
-            # Compute loglikelihood contribution for this data point and this cluster
-            Z[k] += np.log(weights[k])
-            Z[k] -= 1/2. * (num_dim * np.log(2*np.pi) + np.log(np.linalg.det(covs[k])) + exponent_term)
-            
-        # Increment loglikelihood contribution of this data point across all clusters
-        ll += log_sum_exp(Z)
-        
-    return ll
-
-def EM(data, init_means, init_covariances, init_weights, maxiter=1000, thresh=1e-4):
+def EM_alg(data, init_means, init_covariances, init_weights, maxiter=1000, thresh=1e-4):
     means = init_means[:]
     covariances = init_covariances[:]
     weights = init_weights[:]
-    
     num_data = len(data)
     num_dim = len(data[0])
     num_clusters = len(means)
-    
     resp = np.zeros((num_data, num_clusters))
     ll = loglikelihood(data, weights, means, covariances)
     ll_trace = [ll]
-    
+
     for it in range(maxiter):
         print("Iteration %s" % it)
 
-        resp = compute_responsibilities(data, weights, means, covariances)
+        # pi
+        resp = e_step(data, weights, means, covariances)
+        counts = np.sum(resp, axis=0)
+        num_clusters = len(counts)
+        weights = [0.] * num_clusters
+        for k in range(num_clusters):
+            weights[k] = counts[k] / np.sum(counts)
 
-        counts = compute_soft_counts(resp)
+        # mu
+        num_clusters = len(counts)
+        num_data = len(data)
+        means = [np.zeros(len(data[0]))] * num_clusters
+        for k in range(num_clusters):
+            weighted_sum = 0.
+            for i in range(num_data):
+                weighted_sum += data[i] * resp[i][k]
+            means[k] = weighted_sum / counts[k]
 
-        weights = compute_weights(counts)
-      
-        means = compute_means(data, resp, counts)
-        
-        covariances = compute_covariances(data, resp, counts, means)
-        
+        # sigma
+        num_clusters = len(counts)
+        num_dim = len(data[0])
+        num_data = len(data)
+        covariances = [np.zeros((num_dim, num_dim))] * num_clusters
+        for k in range(num_clusters):
+            weighted_sum = np.zeros((num_dim, num_dim))
+            for i in range(num_data):
+                weighted_sum += resp[i][k] * np.outer(data[i] - means[k], data[i] - means[k])
+            covariances[k] = weighted_sum / counts[k]
+
     out = {'weights': weights, 'means': means, 'covs': covariances, 'loglik': ll_trace, 'resp': resp}
     return out
 
-results = EM(X, mu_list, sigma_list, pi_list, maxiter=1)
-# Square figure size
-plt.figure(figsize=(8,8))
 
-plt.plot(X[:,0], X[:,1], 'o', markerfacecolor="None", alpha=0.3)
+results = EM_alg(X, mu_list, sigma_list, pi_list, maxiter=1)
+# Square figure size
+plt.figure(figsize=(8, 8))
+plt.plot(X[:, 0], X[:, 1], 'o', markerfacecolor="None", alpha=0.3)
 colors = ['tab:orange', 'tab:green', 'tab:red', 'tab:purple']
 for i in range(K):
-  plot_gaussian(results['means'][i], results['covs'][i])
+    plot_gaussian(results['means'][i], results['covs'][i])
 
 # Axes
 plt.gca().axhline(y=0, color='gray')
